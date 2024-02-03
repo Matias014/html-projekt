dd6f49bed0367e9d0eb882186e2525e01bbbb8f9
Matias014 <mateusz104104@gmail.com>
2024-02-03 18:10:38 +0100

    jsdoc

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,20 @@
+/**
+ * Obsługuje działanie menu typu "burger".
+ * 
+ * Po załadowaniu całego dokumentu DOM, skrypt szuka elementów DOM odpowiedzialnych za
+ * burger-menu (przycisk menu) oraz za listę linków w menu. Następnie dodaje słuchacza
+ * zdarzeń do przycisku menu burger, który po kliknięciu zmienia klasę CSS linków,
+ * powodując ich ukazanie się lub ukrycie.
+ * 
+ */
 document.addEventListener('DOMContentLoaded', function () {
+    // Znajduje selektor burger-menu w dokumencie css
     const burgerMenu = document.querySelector('.burger-menu');
+    // Znajduje selektor kontenera z linkami menu w dokumencie css
     const linki = document.querySelector('.linki');
 
+    // Dodaje słuchacza zdarzeń do przycisku menu burger, który przełącza klasę 'active'
+    // na kontenerze z linkami, powodując ich pokazanie lub ukrycie.
     burgerMenu.addEventListener('click', function () {
         linki.classList.toggle('active');
     });
--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,14 @@
+/** Funkcja kliknij służy do wyświetlenia komunikatu */
 function kliknij() {
     alert("Świeży i chrupiący chleb!");
 }
 
+/** Funkcja kliknij2 służy do wyświetlenia komunikatu */
 function kliknij2() {
     alert("Najlepszy chleb!");
 }
 
+/** Funkcja kliknij3 służy do wyświetlenia komunikatu */
 function kliknij3() {
     alert("Najtańszy chleb!");
 }
